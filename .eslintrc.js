747b2c5f7ecaa855e1c0135c0d723bfc77ea95f4
Jonny Thaw <hi@jthaw.me>
2020-09-29 19:45:09 +0100

    feat: allow non round numbers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,41 @@
 module.exports = {
-    root: true,
+	root: true,
 
-    env: {
+	env: {
 		node: true
 	},
 
-    plugins: ['pug'],
+	plugins: ["pug"],
 
-    'extends': [
-		'plugin:vue/recommended'
-	],
+	extends: ["plugin:vue/recommended"],
 
-    rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-multiple-empty-lines': [2, { 'max': 2, 'maxEOF': 1 }],
-		'no-tabs': 0,
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-multiple-empty-lines": [2, { max: 2, maxEOF: 1 }],
 		// 'indent': [2, 'tab'],
-		'indent': 'off',
-		'vue/script-indent': ['error', 'tab', {
-			'baseIndent': 1,
-			'switchCase': 1
-		}],
-		'vue/html-indent': ['error', 'tab', {
-			'attribute': 1,
-			'baseIndent': 1,
-			'closeBracket': 0,
-			'alignAttributesVertically': true,
-			'ignores': []
-		}],
-		'vue/component-name-in-template-casing': 'error',
+		"vue/component-name-in-template-casing": "error",
 		// single quotes
-		'quotes': [2, 'single'],
+		quotes: [2, "single"],
 		// allow paren-less arrow functions
-		'arrow-parens': 0,
+		"arrow-parens": 0,
 		// allow async-await
-		'generator-star-spacing': 0
+		"generator-star-spacing": 0
 	},
 
-    parserOptions: {
-		parser: 'babel-eslint'
+	parserOptions: {
+		parser: "babel-eslint"
 	},
 
-    overrides: [
-      {
-        files: [
-          '**/__tests__/*.{j,t}s?(x)',
-          '**/tests/unit/**/*.spec.{j,t}s?(x)'
-        ],
-        env: {
-          jest: true
-        }
-      }
-    ]
-}
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)"
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
+};
